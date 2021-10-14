5ccdfeeaa6ed121bae49d04a5b908b748e2171d6
mapy0 <90832266+mapy0@users.noreply.github.com>
2021-10-14 01:09:12 +0000

    ~Top View

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#-----------Home 画面-------------
+ get 'top' => 'homes#top'
+ root "homes#top" # https://bookers-level1.herokuapp.com/ をhome画面にする。
+ 
+ #-----------Booker  画面-------------
 end
