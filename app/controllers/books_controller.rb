d33e26826b2069e04ffe1bed935e351d92cff83e
mapy0 <90832266+mapy0@users.noreply.github.com>
2021-10-14 06:36:38 +0000

    [finish]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,38 +23,28 @@ class BooksController < ApplicationController
   # POST /books or /books.json
   def create
     @book = Book.new(book_params)
-
-    respond_to do |format|
-      if @book.save
-        format.html { redirect_to @book, notice: "Book was successfully created." }
-        format.json { render :show, status: :created, location: @book }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+    if @book.save
+      
+      redirect_to @book, notice: "Book was successfully created." 
+    else
+       @books = Book.all
+       render 'index'
     end
   end
 
   # PATCH/PUT /books/1 or /books/1.json
   def update
-    respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to @book, notice: "Book was successfully updated." }
-        format.json { render :show, status: :ok, location: @book }
+       redirect_to @book, notice: "Book was successfully updated."
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
+       render :edit, status: :unprocessable_entity
       end
-    end
   end
 
   # DELETE /books/1 or /books/1.json
   def destroy
     @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
-      format.json { head :no_content }
-    end
+      redirect_to books_url, notice: "Book was successfully destroyed."
   end
 
   private
